b83e4aacf8ed022f1fd7971ca0a29f4680945529
shubhamkunwar10 <kshubham@c3ihub.iitk.ac.in>
2024-11-01 13:08:21 +0530

    add interface Block

--- src/interfaces/interface.tsx
+++ src/interfaces/interface.tsx
@@ -114,3 +114,28 @@ export interface NetworkConfig {
   rpcUrls: string[];
   blockExplorerUrls: string[];
 }
+
+// Block interface based on the data structure provided
+export interface Block {
+  baseFeePerGas: string;
+  difficulty: string;
+  extraData: string;
+  gasLimit: string;
+  gasUsed: string;
+  hash: string;
+  logsBloom: string;
+  miner: string;
+  mixHash: string;
+  nonce: string;
+  number: string;
+  parentHash: string;
+  receiptsRoot: string;
+  sha3Uncles: string;
+  size: string;
+  stateRoot: string;
+  timestamp: string;
+  totalDifficulty: string;
+  transactionsRoot: string;
+  uncles: string[];
+  transactions: string[];
+}
